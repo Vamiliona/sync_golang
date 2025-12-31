b7955ab39d0cc1be232676fc6db69b1a18f0ebc2
Vamiliona <166242636+Vamiliona@users.noreply.github.com>
2025-12-31 14:11:37 +0700

    Update database connection to use MySQL environment vars

--- config/database.go
+++ config/database.go
@@ -13,11 +13,11 @@ var DB *gorm.DB
 func ConnectDB() {
 	dsn := fmt.Sprintf(
 		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
-		os.Getenv("DB_USER"),
-		os.Getenv("DB_PASS"),
-		os.Getenv("DB_HOST"),
-		os.Getenv("DB_PORT"),
-		os.Getenv("DB_NAME"),
+		os.Getenv("MYSQL_USER"),
+		os.Getenv("MYSQL_PASSWORD"),
+		os.Getenv("MYSQL_HOST"),
+		os.Getenv("MYSQL_PORT"),
+		os.Getenv("MYSQL_DATABASE"),
 	)
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
